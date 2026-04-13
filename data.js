3a537aa6a5680a472f1608c62b9ee05cabcda328
Alex <alexrichardpoirier@gmail.com>
2026-04-12 22:10:23 -0400

    Adds new initial text collection and filtering script.

--- data.js
+++ data.js
@@ -14,7 +14,20 @@ const origins = {};
     
     const page = new DOMParser().parseFromString(result);
     
-    const urls = page.querySelectorAll("bdi > a").map(element => element.href).filter(url => url.includes("Recipe_"));
+    // Might want to unfliter this at a later date if missing recipes are noticed in the second format (N)
+    const urls = page.querySelectorAll("bdi > a").map(element => element.href).filter(url => url.includes("Recipe/"));
+    
+    for (url of urls) {
+    
+        let response2 = await fetch("https://wiki.hypixel.net/index.php?title=" + url.split("/").at(-1) + "&action=raw");
+        let result2 = await response2.text();
+        
+        // Maybe do the recipe split a bit later
+        let textData = result2.split("|stranded")[0].replace(/^(?!\|(?:in|out)).*(?:\n|)/gm, "").trim().toUpperCase().split(/(?<=\|out.*)\n/gm);
+        
+        
+    
+    }
 }
 
 //--------------------------------------------------------------------------------
@@ -27,7 +40,17 @@ const origins = {};
     
     const page = new DOMParser().parseFromString(result);
     
-    const urls = page.querySelectorAll("bdi > a").map(element => element.href).filter(url => url.includes("Recipe_"));
+    // Might want to unfliter this at a later date if missing recipes are noticed in the second format (Y)
+    const urls = page.querySelectorAll("bdi > a").map(element => element.href).filter(url => url.includes("Recipe/"));
+    
+    for (url of urls) {
+    
+        let response2 = await fetch("https://wiki.hypixel.net/index.php?title=" + url.split("/").at(-1) + "&action=raw");
+        let result2 = await response2.text();
+        
+        // Maybe do the recipe split a bit later
+        let textData = result2.split("|stranded")[0].replace(/^(?!\|(?:in|out|duration)).*(?:\n|)/gm, "").trim().toUpperCase().split(/(?<=\|out.*)\n/gm);;
+    }
 }
 
 //--------------------------------------------------------------------------------
