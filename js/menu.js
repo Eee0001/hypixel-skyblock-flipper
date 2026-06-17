c9f565a412f0a8730f056a382d2455943785647d
Alex <alexrichardpoirier@gmail.com>
2026-06-17 13:41:59 -0400

    Adds menu singleton.

---
+++ js/menu.js
@@ -0,0 +1,99 @@
+//--------------------------------------------------------------------------------
+// IMPORTS
+//--------------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------------
+// CLASS
+//--------------------------------------------------------------------------------
+
+class Menu {
+
+  #elements; #ids; #dataKeys; #previousData;
+
+  constructor () {
+    this.#ids = [
+      "menu", "toggle-menu-btn"
+    ];
+
+    this.#elements = {};
+
+    for (let id of this.#ids) {
+      this.#elements[id] = document.getElementById(id);
+    }
+
+    this.#dataKeys = {};
+
+    this.#previousData = {};
+  }
+
+  //--------------------------------------
+
+  #getDataTargets (app) {
+    return {};
+  }
+
+  //--------------------------------------
+
+  initEvents (app) {
+    for (let [id, dataKey] of Object.entries(this.#dataKeys)) {
+      const element = this.#elements[id];
+      
+      element.onchange = (e) => {
+        const dataTargets = this.#getDataTargets(missionManager);
+
+        const dataTarget = dataTargets[dataKey.target];
+        if (!dataTarget) return; 
+
+        if (element.type === "number") {
+          dataTarget[dataKey.property] = element.valueAsNumber;
+        }
+
+        if (element.type === "select-one") {
+          dataTarget[dataKey.property] = Number(element.value);
+        }
+
+        if (element.type === "checkbox") {
+          dataTarget[dataKey.property] = element.checked;
+        }
+      }
+    }
+
+    this.#elements["toggle-menu-btn"].onclick = ()=>{ 
+      this.#elements["menu"].classList.toggle("hidden"); 
+    };
+  }
+
+  //--------------------------------------
+
+  refresh (app) {
+    if (document.activeElement !== document.body) return;
+
+    const dataTargets = this.#getDataTargets(app);
+
+    for (let [id, dataKey] of Object.entries(this.#dataKeys)) {
+      const dataTarget = dataTargets[dataKey.target];
+      if (!dataTarget) continue;
+      
+      const data = dataTarget[dataKey.property];
+
+      if (this.#previousData[id] !== data) {
+        if (this.#elements[id].type === "checkbox") {
+          this.#elements[id].checked = this.#previousData[id] = data;
+        }
+        else {
+          this.#elements[id].value = this.#previousData[id] = data;
+        }
+      }
+      
+    }
+  }
+  
+}
+
+//--------------------------------------------------------------------------------
+// EXPORTS
+//--------------------------------------------------------------------------------
+
+const menu = new Menu();
+
+//--------------------------------------------------------------------------------
